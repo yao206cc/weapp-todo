e80ab2d2f8f6cee77ffe99ca560a3741817ee1cb
yao206cc <yao206cc@gmail.com>
2020-09-23 17:46:41 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,108 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    taskForm: {
+      content: '',
+      endTime: '',
+      isImportant: true,
+      isRemind: false
+    },
+    multiIndex: [0, 0],
+    multiArray: [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+      ['小时 内完成', '天 内完成', '周 内完成', '月 内完成', '年 内完成']
+    ],
+    rules: [{
+      name: 'content',
+      rules: {
+        required: true,
+        message: '内容是必填项'
+      }
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  formInputChange(e) {
+    const {
+      field
+    } = e.currentTarget.dataset
+    this.setData({
+      [`taskForm.${field}`]: e.detail.value
+    })
+  },
+  submitForm() {
+    this.selectComponent('#form').validate((valid, errors) => {
+      console.log('valid', valid, errors)
+      if (!valid) {
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          this.setData({
+            error: errors[firstError[0]].message
+          })
+
+        }
+      } else {
+        wx.showToast({
+          title: '校验通过'
+        })
+      }
+    })
+  },
+})
